a85f650745ed98210769f7298eaafeed86c67b82
TankEngine-ish <plamendimitrov0308@yahoo.com>
2024-04-15 23:17:05 +0300

    create CORS function

--- backend/main.go
+++ backend/main.go
@@ -38,11 +38,36 @@ func main() {
 		log.Fatal(err)
 	}
 
-	router := mux.NewRouter()
+	router := mux.NewRouter().PathPrefix("/api").Subrouter() // creates a new subrouter so I don't have to put api in front of each path
 	router.HandleFunc("/users", getUsers).Methods("GET")
 	router.HandleFunc("/users", createUser).Methods("POST")
-	router.HandleFunc("/users/{email}", getUser).Methods("GET")
-	router.HandleFunc("/users/{email}", updateUser).Methods("PUT")
-	router.HandleFunc("/users/{email}", deleteUser).Methods("DELETE")
+	router.HandleFunc("/users/{id}", getUser).Methods("GET")
+	router.HandleFunc("/users/{id}", updateUser).Methods("PUT")
+	router.HandleFunc("/users/{id}", deleteUser).Methods("DELETE")
 	log.Fatal(http.ListenAndServe(":8000", router))
+
+	enhancedRouter := enableCORS(jsonContentMiddleware(router))
+
+	log.Fatal(http.ListenAndServe(":8000", enhancedRouter))
 }
+
+//CORS middleware function below
+func enableCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		//the CORS headers
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+		// If the request method is OPTIONS, we're dealing with a preflight request.
+		//This is a request that the browser sends to check if the server allows the request.
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func 
