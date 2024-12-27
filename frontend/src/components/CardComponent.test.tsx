75f70ee905994f3f8f4dd779003b091e94c95514
TankEngine-ish <plamendimitrov0308@yahoo.com>
2024-12-27 21:20:50 +0200

    change name of component test

--- frontend/src/components/CardComponent.test.tsx
+++ frontend/src/components/CardComponent.test.tsx
@@ -4,11 +4,11 @@ import '@testing-library/jest-dom';
 import CardComponent from './CardComponent';
 
 test('renders CardComponent with correct data', () => {
-    const card = { id: 1, name: 'John Doe', statement: 100 };
+    const card = { id: 1, name: 'Spiderman', statement: 100 };
     const { getByText } = render(<CardComponent card={card} />);
 
     expect(getByText('Id: 1')).toBeInTheDocument();
-    expect(getByText('John Doe')).toBeInTheDocument();
+    expect(getByText('Spiderman')).toBeInTheDocument();
     expect(getByText('100')).toBeInTheDocument();
 });
 
