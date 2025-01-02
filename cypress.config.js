703e880f1638004560ab4d42ad4e735dddb05099
TankEngine-ish <plamendimitrov0308@yahoo.com>
2025-01-02 23:21:49 +0200

    add spec pattern to cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -3,9 +3,9 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
+    specPattern: 'cypress/e2e/userExperience.cy.js',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    supportFile: false, // Disable support file
   },
 });
